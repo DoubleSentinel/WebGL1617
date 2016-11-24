b682467ba6d7dd3c5405a18bcbcd336fa30dfcb0
LumenH <aurelie.debrot@he-arc.ch>
2016-11-24 15:48:45 +0100

    JSON file and Matrix class

---
+++ MPs/Aurélie/js/matrix.js
@@ -0,0 +1,11 @@
+class Matrix{
+
+/*dim est un entier, rows un tableau qu'il faudra décomposer*/
+  constructor(dim, rows){
+    this.dim = dim; //représente la dimension de la matrice. On va commencer avec des matrices carrées.
+    this.rows = []//réfléchir à comment représenter cette matrice.
+    for(var i=0; i<dim; i++){
+
+    }
+  }
+}
