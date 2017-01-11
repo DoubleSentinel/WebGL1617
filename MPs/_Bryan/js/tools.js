28fe6bbc3b01786ac6f68009007ce23764b8aaec
BryanMums <bryan.muhmenthaler@gmail.com>
2017-01-11 12:29:35 +0100

    Ajout version moche. Bug vitesse/reste

--- MPs/_Bryan/js/tools.js
+++ MPs/_Bryan/js/tools.js
@@ -1,104 +1,41 @@
-var glContext = null;
-var c_width = 0;
-var c_height = 0;
-var prg = null;
+function getTextContent( elementID ) {
+		var element = document.getElementById(elementID);
+		var fsource = "";
+		var node = element.firstChild;
+		var str = "";
+		while (node) {
+			if (node.nodeType == 3) // this is a text node
+				str += node.textContent;
+			node = node.nextSibling;
+		}
+		return str;
+	}
 
-function degToRad(degrees) {
-    return (degrees * Math.PI / 180.0);
+
+function createProgram(gl, vertexShaderSource, fragmentShaderSource) {
+	   var vsh = gl.createShader( gl.VERTEX_SHADER );
+	   gl.shaderSource(vsh,vertexShaderSource);
+	   gl.compileShader(vsh);
+	   if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+		  console.log("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
+	   }
+	   var fsh = gl.createShader( gl.FRAGMENT_SHADER );
+	   gl.shaderSource(fsh, fragmentShaderSource);
+	   gl.compileShader(fsh);
+	   if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
+		  console.log("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+	   }
+	   var prog = gl.createProgram();
+	   gl.attachShader(prog,vsh);
+	   gl.attachShader(prog, fsh);
+	   gl.linkProgram(prog);
+	   if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
+		  console.log("Link error in program:  " + gl.getProgramInfoLog(prog));
+	   }
+	return prog;
 }
 
-/**
- * Allow to initialize Shaders.
- */
-function getShader(gl, id) {
-    var script = document.getElementById(id);
-    if (!script) {
-        return null;
-    }
 
-    var str = "";
-    var k = script.firstChild;
-    while (k) {
-        if (k.nodeType == 3) {
-            str += k.textContent;
-        }
-        k = k.nextSibling;
-    }
-
-    var shader;
-    if (script.type == "x-shader/x-fragment") {
-        shader = glContext.createShader(glContext.FRAGMENT_SHADER);
-    } else if (script.type == "x-shader/x-vertex") {
-        shader = glContext.createShader(glContext.VERTEX_SHADER);
-    } else {
-        return null;
-    }
-
-    glContext.shaderSource(shader, str);
-    glContext.compileShader(shader);
-
-    if (!glContext.getShaderParameter(shader, glContext.COMPILE_STATUS)) {
-        alert(glContext.getShaderInfoLog(shader));
-        return null;
-    }
-    return shader;
-}
-
-/**
- * The program contains a series of instructions that tell the Graphic Processing Unit (GPU)
- * what to do with every vertex and fragment that we transmit.
- * The vertex shader and the fragment shaders together are called through that program.
- */
-function initProgram() {
-    console.log("On utilise initProgram");
-    var fgShader = getShader(glContext, "shader-fs");
-    var vxShader = getShader(glContext, "shader-vs");
-
-    prg = glContext.createProgram();
-    glContext.attachShader(prg, vxShader);
-    glContext.attachShader(prg, fgShader);
-    glContext.linkProgram(prg);
-
-    if (!glContext.getProgramParameter(prg, glContext.LINK_STATUS)) {
-        alert("Could not initialise shaders");
-    }
-
-    glContext.useProgram(prg);
-
-    initShaderParameters(prg);
-
-}
-
-function requestAnimFrame(o) {
-    requestAnimFrame(o);
-}
-
-/**
- * Provides requestAnimationFrame in a cross browser way.
- */
-requestAnimFrame = (function() {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-            window.setTimeout(callback, 1000.0 / 60.0);
-        };
-})();
-
-/**
- * Render Loop: frame rate and scene drawing.
- */
-function renderLoop() {
-    console.log("Début renderLoop");
-    requestAnimFrame(renderLoop);
-    drawScene();
-}
-
-/**
- * Verify that WebGL is supported by your machine
- */
 function getGLContext(canvasName) {
     var canvas = document.getElementById(canvasName);
     if (canvas == null) {
@@ -139,7 +76,6 @@ function getGLContext(canvasName) {
 }
 
 
-
 /**
  * The following code snippet creates a vertex buffer and binds the vertices to it.
  */
@@ -174,6 +110,7 @@ function getArrayBufferWithArray(values) {
 
     return vBuffer;
 }
+
 
 
 function initTextureWithImage(sFilename, texturen) {
@@ -219,56 +156,12 @@ function initTextureWithImage(sFilename, texturen) {
     glContext.texParameteri(glContext.TEXTURE_2D, glContext.TEXTURE_WRAP_T, glContext.CLAMP_TO_EDGE);
 }
 
-function calculateTangents(vs, tc, ind) {
-    var i;
-    var tangents = [];
-    for (i = 0; i < vs.length / 3; i++) {
-        tangents[i] = [0, 0, 0];
-    }
-    // Calculate tangents
-    var a = [0, 0, 0],
-        b = [0, 0, 0];
-    var triTangent = [0, 0, 0];
-    for (i = 0; i < ind.length; i += 3) {
 
-        var i0 = ind[i + 0];
-        var i1 = ind[i + 1];
-        var i2 = ind[i + 2];
+function renderLoop() {
+    drawScene();
+    refreshTimer = requestAnimationFrame(renderLoop);
+}
 
-        var pos0 = [vs[i0 * 3], vs[i0 * 3 + 1], vs[i0 * 3 + 2]];
-        var pos1 = [vs[i1 * 3], vs[i1 * 3 + 1], vs[i1 * 3 + 2]];
-        var pos2 = [vs[i2 * 3], vs[i2 * 3 + 1], vs[i2 * 3 + 2]];
-
-        var tex0 = [tc[i0 * 2], tc[i0 * 2 + 1]];
-        var tex1 = [tc[i1 * 2], tc[i1 * 2 + 1]];
-        var tex2 = [tc[i2 * 2], tc[i2 * 2 + 1]];
-
-        vec3.subtract(pos1, pos0, a);
-        vec3.subtract(pos2, pos0, b);
-
-        var c2c1t = tex1[0] - tex0[0];
-        var c2c1b = tex1[1] - tex0[1];
-        var c3c1t = tex2[0] - tex0[0];
-        var c3c1b = tex2[0] - tex0[1];
-
-        triTangent = [c3c1b * a[0] - c2c1b * b[0], c3c1b * a[1] - c2c1b * b[1], c3c1b * a[2] - c2c1b * b[2]];
-
-        vec3.add(tangents[i0], triTangent);
-        vec3.add(tangents[i1], triTangent);
-        vec3.add(tangents[i2], triTangent);
-    }
-
-    // Normalize tangents
-    var ts = [];
-    for (i = 0; i < tangents.length; i++) {
-        var tan = tangents[i];
-        vec3.normalize(tan);
-
-        ts.push(tan[0]);
-        ts.push(tan[1]);
-        ts.push(tan[2]);
-
-    }
-
-    return ts;
+function degToRad(degrees) {
+    return (degrees * Math.PI / 180.0);
 }
